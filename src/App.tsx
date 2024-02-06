b0953f217af7a1fce62aaec8f316eb2d8b455ba5
Mateus Villa Nova <villa1999nova@gmail.com>
2024-02-06 11:11:06 -0300

    feat: Basic structure created

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,67 @@
+import logo from './assets/logo.svg'
+
 export function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="" />
+
+      <form className="w-full ">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <div className="rounded-md bg-slate-700 p-5 space-y-3">
+          <span className="text-sm font-medium text-slate-200">
+            Adicionar nota
+          </span>
+          <p className="text-sm leading-6 text-slate-400">
+            Grave uma nota em aúdio que será convertida para texto
+            automaticamente.
+          </p>
+        </div>
+
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+          <span className="text-sm font-medium text-slate-300">há 2 dias</span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+            tempore earum enim eveniet molestias, fugiat pariatur temporibus
+            iusto consequuntur optio similique illum voluptatibus minima
+            obcaecati! Excepturi maiores architecto harum possimus.
+          </p>
+
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
+        </div>
+
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+          <span className="text-sm font-medium text-slate-300">há 4 dias</span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+            neque quia! Aspernatur, in ipsa nulla suscipit aliquam placeat
+            quibusdam ipsum voluptate incidunt veritatis adipisci minus! Sunt
+            nisi saepe unde ipsa?
+          </p>
+
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
+        </div>
+
+        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+          <span className="text-sm font-medium text-slate-300">há 7 dias</span>
+          <p className="text-sm leading-6 text-slate-400">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti
+            vel sint architecto officia magni ipsa quo aliquid omnis iure.
+            Maxime aut nostrum non asperiores vitae. Velit eum maiores
+            repudiandae non?
+          </p>
+
+          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
+        </div>
+      </div>
+    </div>
+  )
 }
