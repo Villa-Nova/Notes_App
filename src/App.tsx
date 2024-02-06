5a33a71655ee5b345b2875c7c4a0659a08da94d5
Mateus Villa Nova <villa1999nova@gmail.com>
2024-02-06 16:13:32 -0300

    feat: Modal component created with the Radix lib

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,12 @@ export function App() {
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
 
-        <NoteCard />
+        <NoteCard
+          note={{
+            date: new Date(),
+            content: 'Mateus Villa Nova',
+          }}
+        />
       </div>
     </div>
   )
